a5766120ba9c44a5ebfd237887f1a98a4cbcb3cc
salaz310 <lrso.10@hotmail.com>
2020-02-28 08:59:21 -0600

    Realizando mas pruebas con GULP

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ const zip = require('gulp-zip');
 exports.default = () => (
     gulp.src('./**')
         .pipe(zip('Tarea3_201213181.zip'))
-        .pipe(gulp.dest('./imgs	'))
+        .pipe(gulp.dest('./ArtefactosFinales'))
 );
