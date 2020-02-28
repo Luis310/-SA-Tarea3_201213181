321e5c3f84756ad1feef5af83db58ad82330f540
salaz310 <lrso.10@hotmail.com>
2020-02-28 08:51:31 -0600

    Se modifico la creacion del archivo zio

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
  
 exports.default = () => (
-    gulp.src('./*')
-        .pipe(zip('archive.zip'))
+    gulp.src('./**')
+        .pipe(zip('Tarea3_201213181.zip'))
         .pipe(gulp.dest('./imgs	'))
 );
