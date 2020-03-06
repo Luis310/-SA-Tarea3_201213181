32371afd74fa1a30b6efa38122ab2ef517fe9bea
salaz310 <lrso.10@hotmail.com>
2020-03-06 09:03:31 -0600

    Se añadio una prueba mas a GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+
+var fileindex= require('gulp-fileindex');
+
  
-exports.default = () => (
+/*exports.default = () => (
     gulp.src('./**')
         .pipe(zip('Tarea3_201213181.zip'))
         .pipe(gulp.dest('./ArtefactosFinales'))
-);
+);*/
+
+gulp.task('zip',function(){
+	return gulp.src('./**')
+		   .pipe(zip('Tarea3_201213181.zip'))
+		   .pipe(gulp.dest('./ArtefactosFinales'));
+});
+
+
+
+gulp.task('fileindex',function(){
+	return gulp.src('./ArtefactosFinales/*.zip')
+		   .pipe(fileindex())
+		   .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default',gulp.series('zip','fileindex'));
